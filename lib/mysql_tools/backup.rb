d380299e9da7194cf8f47c0bc4f4835c075d88cf
Jens Braeuer <braeuer.jens@googlemail.com>
2012-04-08 13:23:15 +0200

    Move command options into command module.

--- lib/mysql_tools/backup.rb
+++ lib/mysql_tools/backup.rb
@@ -1,5 +1,30 @@
 module MysqlTools
   class Backup
+
+    def self.init
+      desc "Backup a database"
+      long_desc <<-EOS
+		Dump the seletected database using 'mysqldump'.
+		EOS
+      arg_name "database_name"
+      command :backup do |c|
+
+        c.desc "Output filename"
+        c.arg_name "FILENAME"
+        c.default_value '#{database_name}-#{timestamp}.sql'
+        c.flag [:'output-file']
+
+        c.desc "Output path"
+        c.arg_name "PATH"
+        c.default_value '.'
+        c.flag [:'output-path']
+
+        c.action do |global,command,args|
+          MysqlTools::Backup.new(global, command,args).run
+        end
+      end
+    end
+
     def self.pre(global, options, args)
       verbose "Backup command, pre check."
       return false if (global[:username].nil? or
@@ -7,6 +32,8 @@ module MysqlTools
                        global[:host].nil?)
       return true
     end
+
+    # ----------------------------------------
 
     def initialize(global, options, args)
       @global, @options, @args = global, options, args
