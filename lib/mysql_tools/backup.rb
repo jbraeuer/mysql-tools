9a495ceacf1b3cae318a39cb482e998a4b17dfd7
Jens Braeuer <jens.braeuer@commercetools.de>
2012-04-11 06:44:42 +0000

    Use mysql defaults-file to avoid passwords in process list.

--- lib/mysql_tools/backup.rb
+++ lib/mysql_tools/backup.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'tempfile'
 
 module MysqlTools
   class Backup
@@ -42,26 +43,50 @@ module MysqlTools
       @global, @options, @args = global, options, args
     end
 
+    def setup_mysqldump(tempfile)
+      content = <<-EOS
+		[client]
+		user="#{@global[:username]}"
+		password="#{@global[:password]}"
+		EOS
+      tempfile.write(content.gsub(/^\t+/, ""))
+      tempfile.size # will flush as side-effect
+
+      [ "mysqldump", "--defaults-file=#{tempfile.path}",
+        # consistent DB dumps
+        "--single-transaction",
+        # make dump "nicer", so my_obfuscate can read it and Ruby does not choke
+        "--complete-insert", "--default-character-set=utf8", "--hex-blob" ]
+    end
+
     def run
+      File.umask 0077
+
       timestamp = Time.now.strftime("%Y%m%d-%H%M%S")
+      tempfile = Tempfile.new("mysql-tools")
 
-      @args.each do |database_name|
-        mysqldump = ["mysqldump",
-                     # consistent DB dumps
-                     "--single-transaction",
-                     # make dump "nicer", so my_obfuscate can read it and Ruby does not choke
-                     "--complete-insert", "--default-character-set=utf8", "--hex-blob",
-                     "--user=#{@global[:username]}", "--password=#{@global[:password]}", database_name]
-        gzip = ["nice", "pigz"]
+      begin
+        verbose "Using tempfile #{tempfile.path}"
+        mysqldump = setup_mysqldump(tempfile)
+        compress = ["nice", "pigz"]
 
-        output_file = eval( '"' + @options[:'output-file'] + '"' )
-        output = File.join(@options[:'output-path'], output_file)
+        @args.each do |database_name|
+          output_file = eval( '"' + @options[:'output-file'] + '"' )
+          output = File.join(@options[:'output-path'], output_file)
 
-        log "Will backup database '#{database_name}'. Output to #{output}"
-        File.umask 0077
-        Open3.pipeline(mysqldump, gzip, :out => output, :in => "/dev/null").each do |s|
-          raise "Process failed: #{s.exitstatus}" unless s.exitstatus == 0
+          log "Will backup database '#{database_name}'. Output to #{output}"
+          verbose "Will run: #{mysqldump.inspect}; #{compress.inspect}"
+          Open3.pipeline(mysqldump + [database_name], compress, :out => output, :in => "/dev/null").each do |s|
+            unless s.exitstatus == 0
+              File.unlink(output)
+              raise "Process failed: #{s.exitstatus}"
+            end
+          end
         end
+      ensure
+        # make sure tempfile does not survive
+        tempfile.close
+        tempfile.unlink
       end
     end
   end
